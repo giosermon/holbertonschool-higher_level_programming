4eba48c149532a11720a788bfa817d8957cbe4dd
giosermon <2172@holbertonschool.com>
2021-04-27 19:01:36 -0500

    hot-fix

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,9 +4,9 @@ number = random.randint(-10, 10)
 
 if number > 0:
     print("{:d} is positive".format(number))
-    
+
 elif number < 0:
     print("{:d} is negative".format(number))
-    
+
 else:
     print("{:d} is zero".format(number))
