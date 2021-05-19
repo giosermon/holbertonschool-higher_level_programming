225cc71c7c87c7993919b1b286cc5908f42efbf4
giosermon <2172@holbertonschool.com>
2021-05-18 20:09:42 -0500

    hot fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,14 +3,13 @@
 
 
 class Square:
+    """Class square"""
+    __size = 0
 
     def __init__(self, size=0):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        """Initialize a new Square."""
         self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
