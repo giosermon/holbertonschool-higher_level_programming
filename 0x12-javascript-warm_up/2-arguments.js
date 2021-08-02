9b6168959ca2382ef209fa4e9e35874f85e5ad80
giosermon <2172@holbertonschool.com>
2021-08-02 16:17:26 -0500

    update error of semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,9 @@
 const args = process.argv.length;
 if (args <= 2) {
   console.log('No argument');
-} 
+}
 if (args === 3) {
   console.log('Argument found');
-}
-else if (args > 3) {
+} else if (args > 3) {
   console.log('Arguments found');
 }
