94b1ac5ddec0b121cd110882a0b49bba3f04902a
giosermon <2172@holbertonschool.com>
2021-08-02 19:37:13 -0500

    add update task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 // Write a script that searches the second biggest integer in the list of arguments.
 const args = process.argv;
-if (args <= 3) {
+if (args.lenght <= 3) {
   console.log(0);
 } else {
-  const list = args.sort();
-  console.log(list.reverse()[1]);
+    args.sort((a, b) => a - b);
+    args.pop();
+    console.log(args.pop());
 }
