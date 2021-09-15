bab8ea229eb551e07520d0dbccff03d902a5ddf2
giosermon <2172@holbertonschool.com>
2021-09-14 22:22:30 -0500

    update task

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (err, httpResponse, body) {
-  console.log(JSON.parse(body).title || err);
+const process = require('process');
+
+const movieId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+request(url, function (err, response, body) {
+  if (err) console.log(err);
+  console.log(JSON.parse(body).title);
 });
